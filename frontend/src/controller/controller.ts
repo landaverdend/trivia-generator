f0e923e55a21882b492dbff5ac1cb04e550dc1dc
Nicodemus  Landaverde <nlandaverde@Nicodemuss-Air.lan>
2025-02-22 00:14:26 -0500

    adding env file and updating stuff.

--- frontend/src/controller/controller.ts
+++ frontend/src/controller/controller.ts
@@ -1,15 +1,20 @@
 import express from 'express';
 import { Request, Response } from 'express';
-import { PostBody } from '../types/api';
+import { TriviaResponse, PostBody, ErrorResponse } from '../types/api';
 const port = 3000;
 const app = express();
+
 app.use(express.json());
 
-app.post('/api/generateTrivia', (req: Request<{}, {}, PostBody>, res: Response) => {
-  const str = JSON.stringify(req.body);
+app.post('/api/generateTrivia', (req: Request<{}, {}, PostBody>, res: Response<TriviaResponse | ErrorResponse>) => {
+  if (!req.body.categories || req.body.categories.size == 0) {
+    res.status(400).json({ error: 'Missing field: category' });
+  }
 
-  console.log(str);
-  res.send('Hello, World!');
+  if (!req.body.triviaRounds || !req.body.numQuestions) {
+    res.status(400).json({ error: 'Missing field: triviaRounds or numQuestions' });
+  }
+
 });
 
 app.listen(port, () => {
--- frontend/src/types/api.d.ts
+++ frontend/src/types/api.d.ts
@@ -1,8 +1,16 @@
-
 export type PostBody = {
-
   numQuestions: number;
   triviaRounds: number;
   categories: Set<string>;
   shouldIncludeSources?: boolean;
-}
+};
+
+export type ErrorResponse = {
+  error: string;
+};
+
+export type TriviaResponse = {
+  question: string;
+  answer: string;
+  source?: string;
+};
